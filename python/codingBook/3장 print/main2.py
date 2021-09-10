c5ad96c397a1511e989245ae7d82269a807522d4
dominod <76812646+dbdomino@users.noreply.github.com>
2021-09-10 09:51:41 +0900

    print문

---
+++ python/codingBook/3장 print/main2.py
@@ -0,0 +1,24 @@
+print("ABCDE","FGHIJ")
+print(10, 20)
+print("ABCDE"+"FGHIJ")
+print("ABCDE", 5)
+#print("ABCDE"+ 5) error
+
+print("+------------------------------+")
+print('ABCDE' * 3)
+print(10* 20)
+print('ABC,DEF', end = ',')
+print('GHI', 'JKL', sep=',')
+print('1234566789',"00123", sep='78')
+print('1234566789',"00123")
+print('모모모', end='')
+print('도도오')
+
+print("+------------------------------+")
+print('\n')
+print('ABCDE' * 3)
+print("+------------------------------+")
+print('')
+print('ABCDE' * 3)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
