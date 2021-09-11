fe4757ce346313539bc9e26d6a61c3e661517340
dominod <76812646+dbdomino@users.noreply.github.com>
2021-09-11 23:46:48 +0900

    조건문

---
+++ python/codingBook/11장 조건문/elif 문.py
@@ -0,0 +1,9 @@
+a = int(input())
+if a % 4 == 0:
+    print('mod 0')
+elif a % 4 == 1:
+    print('mod 1')
+elif a % 4 == 2:
+    print('mod 2')
+else:
+    print('mod 3')
---
+++ python/codingBook/11장 조건문/if 문.py
@@ -0,0 +1,10 @@
+a = 5
+b = 10
+if a >= 0:
+    print('a >= 0')
+if a != b:  # 동등비교
+    print(a, '!=', b, sep = '')
+if (a+b) % 2 == 0:
+    print('a+b는 짝수')
+else:
+    print('a+b는 홀수')
